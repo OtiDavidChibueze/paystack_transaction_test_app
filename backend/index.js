a908fc4657dd271b65d2898daee9df1fedc42310
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-09-25 23:30:22 +0100

    fix: assign cor to vercel endpoint

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://paystack-transaction-test-app.vercel.app/",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
